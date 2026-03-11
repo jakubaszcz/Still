7e616fca65328559eef8b569a8185e083e9ed5d5
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-03-11 21:05:42 +0100

    fix: move the mouse movement logic into a separate function

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,16 +11,15 @@ var camera_rotation_x: float = 0.0
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
-func _input(event):
+func _mouse_movement(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * sensivity)
 		camera_rotation_x -= event.relative.y * sensivity
 		camera_rotation_x = clamp(camera_rotation_x, deg_to_rad(-89.0), deg_to_rad(89.0))
 		camera.rotation.x = camera_rotation_x
-		
-		if event.is_action_pressed("ui_cancel"):
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _input(event):
+	_mouse_movement(event)
 
 func _physics_process(_delta: float) -> void:
 	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
