5bc79ad63e8fbf2ee92f904489157f2721149c89
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-03-11 20:55:28 +0100

    feat: create item class interface

--- scripts/handler/NoiseManager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-signal noise_detected(event: NoiseEvent)
-
-func _ready():
-	NoiseManager.noise_detected.connect(_on_noise_detected)
-
-func _on_noise_detected(event : NoiseEvent):
-	print(str(event.intensity) + "," + str(event.NoiseType) + "," + str(event.position))
---
+++ scripts/items/class/item.gd
@@ -0,0 +1,13 @@
+class_name Item
+
+enum ItemType { ITEM }
+
+@export var item_name: ItemType = ItemType.ITEM
+@export var noise_on_drop: float = 0.5
+@export var noise_on_pickup: float = 0.2
+
+func _hold():
+	print("Holding item" + str(item_name))
+	
+func _drop():
+	print("Dropping item" + str(item_name))
