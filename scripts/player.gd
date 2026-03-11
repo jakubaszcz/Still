9f076f079f21b056dd838f0e8f8f301afaa8d8bd
jakub <jakub.szczucinski@epitech.eu>
2026-03-11 11:35:10 +0100

    feat: add mouse movement for the player (FPS)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,31 @@
 extends CharacterBody3D
 
-@export var speed = 14
-@onready var body = $Body
+@export var speed: int = 14
+@onready var body: CSGCylinder3D = $Body
+@onready var sensivity: float = 0.002
+@onready var camera: Camera3D = $Camera3D
 
-var target_velocity = Vector3.ZERO
+var target_velocity: Vector3 = Vector3.ZERO
+var camera_rotation_x: float = 0.0
 
-func _physics_process(delta: float) -> void:
-	var direction = Vector3.ZERO
-	
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	
-	if Input.is_action_pressed("move_backward"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * sensivity)
+		camera_rotation_x -= event.relative.y * sensivity
+		camera_rotation_x = clamp(camera_rotation_x, deg_to_rad(-89.0), deg_to_rad(89.0))
+		camera.rotation.x = camera_rotation_x
 		
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+		if event.is_action_pressed("ui_cancel"):
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(_delta: float) -> void:
+	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var movement_dir: Vector3 = transform.basis * Vector3(input.x, 0, input.y)
+		
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
 	
-	velocity = target_velocity
 	move_and_slide()
